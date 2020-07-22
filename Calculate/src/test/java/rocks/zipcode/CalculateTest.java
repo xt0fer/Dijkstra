6074175a84c92e478caa4a3e7e20a3c4f9b8558a
Kristofer <kris@youngers.org>
2020-07-22 13:37:01 -0400

    added demo test case

--- Calculate/src/test/java/rocks/zipcode/CalculateTest.java
+++ Calculate/src/test/java/rocks/zipcode/CalculateTest.java
@@ -71,4 +71,15 @@ public class CalculateTest {
         assertEquals(expected, actual, 0.0001);
     }
 
+    @org.junit.Test
+    public void runLongerDemo() {
+        Calculate engine = new Calculate();
+
+        String args2 = "( ( ( 4 + 5 ) * ( 7 / 3.0 ) ) - ( 13 / 6.0 ) )";
+        double expected = 18.833333333;
+        double actual = engine.run(args2);
+        assertEquals(expected, actual, 0.0001);
+    }
+
+
 }
