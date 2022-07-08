1fabcd8ac94d2fe72e3a65d22166ac0fc3084ab6
Kristofer <kris@youngers.org>
2022-07-08 16:10:12 -0400

    updated

---
+++ Calculate/src/test/java/rocks/zipcode/CalculateTest2.java
@@ -0,0 +1,16 @@
+package rocks.zipcode;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by kristofer on 10/30/20.
+ */
+public class CalculateTest2 {
+    @Test
+    public void run() throws Exception {
+
+    }
+
+}
